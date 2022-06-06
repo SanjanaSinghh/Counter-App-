03bb2f2527a03ee00c5006eccb8d45a980e3e877
SanjanaSinghh <sanjana.kolkata13@gmail.com>
2022-06-06 12:46:16 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import Counter from "./Count/Counter";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h2>COUNTER</h2>
+      <Counter counter={0} />
     </div>
   );
 }
